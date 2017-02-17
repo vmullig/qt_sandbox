ba705fa60316273a3c345c1e28efb60fae7699a8
Vikram K. Mulligan <vmullig@u.washington.edu>
2017-02-17 01:37:06 -0800

    Working on Test2.

--- dr_mulligan/Test2/Test2/workingarea3.h
+++ dr_mulligan/Test2/Test2/workingarea3.h
@@ -1,4 +1,14 @@
 #ifndef WORKINGAREA3_H
+
+class WorkingArea3 : public QWidget
+{
+    Q_OBJECT
+public:
+    WorkingArea3() {}
+
+    virtual ~WorkingArea3() {}
+};
+
 #define WORKINGAREA3_H
 
 #endif // WORKINGAREA3_H
